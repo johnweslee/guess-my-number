430f32fc9817b26f1efde4bfe041b74bdc019907
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-19 18:56:45 +0530

    update : event listeners

--- script.js
+++ script.js
@@ -32,3 +32,52 @@ function disableButton(buttonSelector, disableBooleanValue){
 function setElementBackgroundColor(elementSelector, colorValue){
     document.querySelector(elementSelector).style.backgroundColor = colorValue;
 }
+
+document.querySelector(guessNumberInputElementSelector).addEventListener('change',function(){
+    inputNumber = Number(document.querySelector('.guess').value);
+});
+
+document.querySelector(againButtonElementSelector).addEventListener('click', function(){
+    setElementBackgroundColor('body','Black');
+    disableButton(checkButtonElementSelector, false);
+    disableButton(againButtonElementSelector, true);
+    setElementContent(scoreElementSelector, defaultScore);
+    secretNumber = getSecretNumber();
+    setElementContent(secretNumberElementSelector, '?');
+});
+
+document.querySelector(checkButtonElementSelector).addEventListener('click', function(){
+    if(!(gameScore < 1)){
+        if(!(inputNumber >= 0 && inputNumber <= 9)){
+            setElementContent(displayMessageElementSelector, 'Enter a number between 0 and 9 only!');
+        }else{
+            if(inputNumber !== secretNumber){
+                if(inputNumber > secretNumber){
+                    setElementContent(displayMessageElementSelector, 'Your guess is too high!');
+                }
+                else{
+                    setElementContent(displayMessageElementSelector, 'Your guess is too low!');
+                }
+                gameScore--;
+                setElementContent(scoreElementSelector, gameScore);
+            }else{
+                setElementContent(secretNumberElementSelector, secretNumber);
+                setElementBackgroundColor('body','Green');
+                setElementContent(displayMessageElementSelector, 'Correct guess!');
+                disableButton(checkButtonElementSelector, true);
+                disableButton(againButtonElementSelector, false);
+                if(gameScore >= highScore){
+                    highScore = gameScore;
+                    setElementContent(highScoreElementSelector, highScore);
+                } 
+            }
+        }
+    }else{
+        setElementContent(displayMessageElementSelector, 'Game over!');
+        setElementBackgroundColor('body','Red');
+        disableButton(checkButtonElementSelector, true);
+        disableButton(againButtonElementSelector, false);
+        gameScore = defaultScore;
+    }    
+});
+
