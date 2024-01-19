16f807689fe1eb0a68a83b4d627dab917e8e2a20
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-19 18:56:09 +0530

    update : required methods

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ let checkButtonElementSelector = '.check',
         highScoreElementSelector = '.highscore',
     secretNumberElementSelector = '.number',
 guessNumberInputElementSelector = '.guess';
+
+const defaultScore = Number(document.querySelector(scoreElementSelector).textContent);
+let gameScore = defaultScore;
+let highScore = Number(document.querySelector(highScoreElementSelector).textContent);
+
+let secretNumber = getSecretNumber(),
+inputNumber = Number(document.querySelector('.guess').value);
+
+function getSecretNumber() {
+  let randomSecretNumber = Math.trunc(Math.random() * 10);
+  console.log(randomSecretNumber);
+  return randomSecretNumber;
+}
+
+function setElementContent(elementSelector, contentValue){
+    document.querySelector(elementSelector).textContent = contentValue;
+}
+
+function disableButton(buttonSelector, disableBooleanValue){
+    document.querySelector(buttonSelector).disabled = disableBooleanValue;
+}
+
+function setElementBackgroundColor(elementSelector, colorValue){
+    document.querySelector(elementSelector).style.backgroundColor = colorValue;
+}
