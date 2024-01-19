34887972d16445e8309b47ddc5e1b494f9346c07
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-19 18:55:29 +0530

    update : element class values

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let checkButtonElementSelector = '.check',
+    againButtonElementSelector = '.again',
+        displayMessageElementSelector = '.message',
+            scoreElementSelector = '.score',
+        highScoreElementSelector = '.highscore',
+    secretNumberElementSelector = '.number',
+guessNumberInputElementSelector = '.guess';
